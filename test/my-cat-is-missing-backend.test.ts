d034db94337f8d638a6f693bf463e7f1fa587753
thomas <tgiroulson@gmail.com>
2023-06-23 14:49:53 +0100

    Initial commit

---
+++ test/my-cat-is-missing-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MyCatIsMissingBackend from '../lib/my-cat-is-missing-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/my-cat-is-missing-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MyCatIsMissingBackend.MyCatIsMissingBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});

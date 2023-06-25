a7b1e4be96ae15a140bbe8854532356d7ef1e74e
thomas <tgiroulson@gmail.com>
2023-06-25 16:51:21 +0100

    Added lambdas to infrastructure.

--- lib/my-cat-is-missing-backend-stack.ts
+++ lib/my-cat-is-missing-backend-stack.ts
@@ -1,9 +1,40 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 
 export class MyCatIsMissingBackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // Create report lambda function
+    const createReportLambda = new lambda.Function(this, 'createReportLambda', {
+      functionName: 'create-report',
+      code: new lambda.AssetCode('src'),
+      handler: './lambda/create-report/handler.handler',
+      runtime: lambda.Runtime.NODEJS_18_X,
+      memorySize: 128,
+      timeout: cdk.Duration.seconds(10)
+    });
+
+    // Get page lambda function
+    const getPageLambda = new lambda.Function(this, 'getPageLambda', {
+      functionName: 'get-page',
+      code: new lambda.AssetCode('src'),
+      handler: './lambda/get-page/handler.handler',
+      runtime: lambda.Runtime.NODEJS_18_X,
+      memorySize: 128,
+      timeout: cdk.Duration.seconds(10)
+    });
+
+    // Get report lambda
+    const getReportLambda = new lambda.Function(this, 'getReportLambda', {
+      functionName: 'get-report',
+      code: new lambda.AssetCode('src'),
+      handler: './lambda/get-report/handler.handler',
+      runtime: lambda.Runtime.NODEJS_18_X,
+      memorySize: 128,
+      timeout: cdk.Duration.seconds(10)
+    });
 
   }
 }

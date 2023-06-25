77339fef09cbcf47b7fc9f4d34c9b99adff2050b
thomas <tgiroulson@gmail.com>
2023-06-25 23:34:33 +0100

    Created RDS database using PostgreSQL with initialisation code. This implementation roughly follows the following tutorial: https://aws.amazon.com/blogs/infrastructure-and-automation/use-aws-cdk-to-initialize-amazon-rds-instances/

--- init/index.ts
+++ init/index.ts
@@ -1,3 +1,31 @@
-export const handler = async (event: any) => {
+import fs from "fs";
+import path from "path";
+import { Client } from "pg";
 
+export const handler = async (event: any, context: any): Promise<any> => {
+
+    try {
+        const host = process.env.DB_ENDPOINT_ADDRESS || '';
+        const database = process.env.DB_NAME || '';
+        const password = process.env.DB_PASSWORD || '';
+        const user = process.env.DB_USER || '';
+
+        const client = new Client({
+            user,
+            password,
+            host,
+            database,
+            port: 5432,
+        });
+
+        await client.connect();
+
+        const sql = fs.readFileSync(path.join(__dirname, 'script.sql')).toString();
+
+        await client.query(sql);
+        await client.end();
+
+    } catch (err) {
+        console.error('Error initialising RDS database.');
+    }
 }
--- lib/my-cat-is-missing-backend-stack.ts
+++ lib/my-cat-is-missing-backend-stack.ts
@@ -1,40 +1,86 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import { aws_apigateway } from 'aws-cdk-lib';
+import {aws_apigateway} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as rds from "aws-cdk-lib/aws-rds";
+import {SubnetType} from "aws-cdk-lib/aws-ec2";
+import {DockerImageCode} from "aws-cdk-lib/aws-lambda";
+import {Role, ServicePrincipal} from "aws-cdk-lib/aws-iam";
+import {AwsCustomResource, AwsCustomResourcePolicy, AwsSdkCall, PhysicalResourceId} from "aws-cdk-lib/custom-resources";
 
 export class MyCatIsMissingBackendStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
+        // In a normal production environment, the use of hard-coded database credentials is strongly advised against.
+        // A more appropriate solution would be to use the SecretsManager but for the purposes of this assignment this
+        // will be sufficient.
+
+        const databaseName = 'mycatismissing';
+        const databaseUsername = 'postgres';
+        const databasePassword = 'Q5xtBNrQu3jC';
+
+        // A VPC is required to allow lambda functions and the RDS instance to connect.
+        const vpc = new ec2.Vpc(this, 'VpcLambda', {
+            maxAzs: 2,
+            natGateways: 0,
+            subnetConfiguration: [
+                {
+                    cidrMask: 24,
+                    name: 'egress',
+                    subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+                },
+                {
+                    cidrMask: 24,
+                    name: 'isolated',
+                    subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+                },
+            ],
+        });
+
+        // Security group for lambda functions
+        const lambdaSecurityGroup = new ec2.SecurityGroup(this, 'LambdaSecurityGroup', {
+            vpc,
+        });
+
         // Create report lambda function
-        const createReportLambda = new lambda.Function(this, 'createReportLambda', {
+        const createReportLambda = new lambda.Function(this, 'CreateReportLambda', {
             functionName: 'create-report',
             code: new lambda.AssetCode('src'),
             handler: './lambda/create-report/handler.handler',
             runtime: lambda.Runtime.NODEJS_18_X,
             memorySize: 128,
-            timeout: cdk.Duration.seconds(10)
+            timeout: cdk.Duration.seconds(10),
+            vpc,
+            vpcSubnets: {subnetType: SubnetType.PRIVATE_WITH_EGRESS},
+            securityGroups: [lambdaSecurityGroup]
         });
 
         // Get page lambda function
-        const getPageLambda = new lambda.Function(this, 'getPageLambda', {
+        const getPageLambda = new lambda.Function(this, 'GetPageLambda', {
             functionName: 'get-page',
             code: new lambda.AssetCode('src'),
             handler: './lambda/get-page/handler.handler',
             runtime: lambda.Runtime.NODEJS_18_X,
             memorySize: 128,
-            timeout: cdk.Duration.seconds(10)
+            timeout: cdk.Duration.seconds(10),
+            vpc,
+            vpcSubnets: {subnetType: SubnetType.PRIVATE_WITH_EGRESS},
+            securityGroups: [lambdaSecurityGroup]
         });
 
         // Get report lambda
-        const getReportLambda = new lambda.Function(this, 'getReportLambda', {
+        const getReportLambda = new lambda.Function(this, 'GetReportLambda', {
             functionName: 'get-report',
             code: new lambda.AssetCode('src'),
             handler: './lambda/get-report/handler.handler',
             runtime: lambda.Runtime.NODEJS_18_X,
             memorySize: 128,
-            timeout: cdk.Duration.seconds(10)
+            timeout: cdk.Duration.seconds(10),
+            vpc,
+            vpcSubnets: {subnetType: SubnetType.PRIVATE_WITH_EGRESS},
+            securityGroups: [lambdaSecurityGroup]
         });
 
         // Create Report API
@@ -64,7 +110,75 @@ export class MyCatIsMissingBackendStack extends cdk.Stack {
 
         getReportApi.root.addResource('report').addMethod('GET');
 
+        // Create a security group for the RDS database
+        const dbSecurityGroup = new ec2.SecurityGroup(this, 'DbSecurityGroup', {
+            vpc,
+        });
 
+        // Database instance
+        const dbInstance = new rds.DatabaseInstance(this, 'DbInstance', {
+            engine: rds.DatabaseInstanceEngine.postgres({ version: rds.PostgresEngineVersion.VER_14_3 }),
+            instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
+            vpc,
+            vpcSubnets: vpc.selectSubnets({
+                subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+            }),
+            databaseName,
+            securityGroups: [dbSecurityGroup],
+            credentials: rds.Credentials.fromPassword(databaseUsername, cdk.SecretValue.unsafePlainText(databasePassword)),
+            allocatedStorage: 20
+        });
 
+        // Docker image function for initialising the database
+        const dockerImageFunction = new lambda.DockerImageFunction(this, 'ResourceInitialiserLambda', {
+            memorySize: 128,
+            functionName: 'ResourceInitialiserLambda',
+            code: DockerImageCode.fromImageAsset(`${__dirname}/../init`, {}),
+            vpc,
+            vpcSubnets: vpc.selectSubnets({
+                subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+            }),
+            securityGroups: [lambdaSecurityGroup],
+            timeout: cdk.Duration.minutes(3),
+            allowAllOutbound: true,
+            environment: {
+                DB_ENDPOINT_ADDRESS: dbInstance.dbInstanceEndpointAddress,
+                DB_NAME: databaseName,
+                DB_PASSWORD: databasePassword,
+                DB_USER: databaseUsername
+            }
+        });
+
+        // Allow ingress on TCP port 5432 (postgres)
+        dbSecurityGroup.addIngressRule(
+            lambdaSecurityGroup,
+            ec2.Port.tcp(5432),
+            'Lambda to Postgres database'
+        );
+
+        // Invoke the initialiser upon stack updates granting the necessary permissions
+        const customResourceFnRole = new Role(this, 'AwsCustomResourceRole', {
+            assumedBy: new ServicePrincipal('lambda.amazonaws.com')
+        });
+
+        dockerImageFunction.grantInvoke(customResourceFnRole);
+
+        const sdkCall: AwsSdkCall = {
+            service: 'Lambda',
+            action: 'invoke',
+            parameters: {
+                FunctionName: dockerImageFunction.functionName,
+            },
+            physicalResourceId: PhysicalResourceId.of(`${id}-AwsSdkCall-${dockerImageFunction.currentVersion.version}`)
+        }
+
+        const customResource = new AwsCustomResource(this, 'AwsCustomResource', {
+            policy: AwsCustomResourcePolicy.fromSdkCalls({ resources: AwsCustomResourcePolicy.ANY_RESOURCE }),
+            onUpdate: sdkCall,
+            timeout: cdk.Duration.minutes(3),
+            role: customResourceFnRole
+        });
+
+        customResource.node.addDependency(dbInstance);
     }
 }

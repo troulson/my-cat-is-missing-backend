dd7abae1b9ee82ce1f91c5774f5d2a35d860287d
thomas <tgiroulson@gmail.com>
2023-06-25 17:05:06 +0100

    Added API routing for lambdas via API Gateway.

--- lib/my-cat-is-missing-backend-stack.ts
+++ lib/my-cat-is-missing-backend-stack.ts
@@ -1,40 +1,70 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { aws_apigateway } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 export class MyCatIsMissingBackendStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // Create report lambda function
-    const createReportLambda = new lambda.Function(this, 'createReportLambda', {
-      functionName: 'create-report',
-      code: new lambda.AssetCode('src'),
-      handler: './lambda/create-report/handler.handler',
-      runtime: lambda.Runtime.NODEJS_18_X,
-      memorySize: 128,
-      timeout: cdk.Duration.seconds(10)
-    });
+        // Create report lambda function
+        const createReportLambda = new lambda.Function(this, 'createReportLambda', {
+            functionName: 'create-report',
+            code: new lambda.AssetCode('src'),
+            handler: './lambda/create-report/handler.handler',
+            runtime: lambda.Runtime.NODEJS_18_X,
+            memorySize: 128,
+            timeout: cdk.Duration.seconds(10)
+        });
 
-    // Get page lambda function
-    const getPageLambda = new lambda.Function(this, 'getPageLambda', {
-      functionName: 'get-page',
-      code: new lambda.AssetCode('src'),
-      handler: './lambda/get-page/handler.handler',
-      runtime: lambda.Runtime.NODEJS_18_X,
-      memorySize: 128,
-      timeout: cdk.Duration.seconds(10)
-    });
+        // Get page lambda function
+        const getPageLambda = new lambda.Function(this, 'getPageLambda', {
+            functionName: 'get-page',
+            code: new lambda.AssetCode('src'),
+            handler: './lambda/get-page/handler.handler',
+            runtime: lambda.Runtime.NODEJS_18_X,
+            memorySize: 128,
+            timeout: cdk.Duration.seconds(10)
+        });
 
-    // Get report lambda
-    const getReportLambda = new lambda.Function(this, 'getReportLambda', {
-      functionName: 'get-report',
-      code: new lambda.AssetCode('src'),
-      handler: './lambda/get-report/handler.handler',
-      runtime: lambda.Runtime.NODEJS_18_X,
-      memorySize: 128,
-      timeout: cdk.Duration.seconds(10)
-    });
+        // Get report lambda
+        const getReportLambda = new lambda.Function(this, 'getReportLambda', {
+            functionName: 'get-report',
+            code: new lambda.AssetCode('src'),
+            handler: './lambda/get-report/handler.handler',
+            runtime: lambda.Runtime.NODEJS_18_X,
+            memorySize: 128,
+            timeout: cdk.Duration.seconds(10)
+        });
 
-  }
+        // Create Report API
+        const createReportApi = new aws_apigateway.LambdaRestApi(this, 'CreateReportApi', {
+            handler: createReportLambda,
+            proxy: false,
+            description: 'API for create-report',
+        });
+
+        createReportApi.root.addResource('create').addMethod('POST');
+
+        // Get Page API
+        const getPageApi = new aws_apigateway.LambdaRestApi(this, 'GetPageApi', {
+            handler: getPageLambda,
+            proxy: false,
+            description: 'API for get-page',
+        });
+
+        getPageApi.root.addResource('page').addMethod('GET');
+
+        // Get Report API
+        const getReportApi = new aws_apigateway.LambdaRestApi(this, 'GetReportApi', {
+            handler: getReportLambda,
+            proxy: false,
+            description: 'API for get-report',
+        });
+
+        getReportApi.root.addResource('report').addMethod('GET');
+
+
+
+    }
 }
